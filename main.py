98745cd32d95bf3fd6caefca66a4b714e0ff756b
Jean-Pierre STEIN <stein.jean-pierre@mail.be>
2022-08-31 22:42:23 +0200

    test

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 import pandas as pd
 import requests as rq
 from bs4 import BeautifulSoup as bs
+from urllib.error import HTTPError
+import re
 
 # Variables
 WEBSITE = "http://books.toscrape.com/"
+# type the book of url book
+urlbook = "http://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html"
 
 HEADER_CSV = ["product_page_url","universal_ product_code (upc)","title","price_including_tax","price_excluding_tax",
               "number_available","product_description","category","review_rating","image_url"]
@@ -20,23 +24,62 @@ COLUMNS_URL = {"universal_ product_code (upc)" :"UPC",
                "image_url" : "image_container"}
 
 # Fonctions
-def analyze_url_book(url,col_url):
+def search_col(th,td,col_url,cols,ind):
+    str = ""
+    i = 0
+    for t in th:
+        if col_url[cols[ind]] == t.string:
+            break
+        else:
+            i = i + 1
+
+    j = 0
+    for t1 in td:
+        if j == i:
+            str = t1.string
+            break
+        else:
+            j = j + 1
+    return str
+
+def analyze_url_book(url,col_url,cols):
     simple_line = []
+    page = rq.get(url)
+
+    soup = bs(page.content, 'html.parser')
+    #print(soup.find_all('li'))
+    th = soup.find_all('th')
+    td = soup.find_all('td')
+    # add url
+    simple_line.append(url)
+    # add UPC
+    simple_line.append(search_col(th, td, col_url, cols, 1))
+    # add title
+    h1 = soup.find_all('h1')
+    for title in h1:
+        simple_line.append(title.string)
+    # add price_excluding_tax
+    simple_line.append(search_col(th, td, col_url, cols, 4))
+    # add price_including_tax
+    simple_line.append(search_col(th, td, col_url, cols, 3))
+    # add number_available
+    nbr_books = search_col(th, td, col_url, cols, 5)
+    simple_line.append(re.sub("[^0-9]", "", nbr_books))
 
     return simple_line
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    page = rq.get(WEBSITE)
-    if page.ok:
-        print("ok")
-    soup = bs(page.content, 'html.parser')
-    print(soup)
+    try:
+        page = rq.get(WEBSITE)
+        page.raise_for_status()
+    except HTTPError as hp:
+        print(hp)
 
     # create one csv for one book
     df = pd.DataFrame(columns=HEADER_CSV)
-    # type the book of url book
-    urlbook = "http://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html"
 
-    df.loc[len(df)] = analyze_url_book(urlbook,COLUMNS_URL)
-
+    #df.loc[len(df)] = analyze_url_book(urlbook,COLUMNS_URL)
+    sl = analyze_url_book(urlbook,COLUMNS_URL,HEADER_CSV)
+    print(sl)
+    #print(df)
