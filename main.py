ea299e9750c1d41d0d4b38fa5224754112d55a37
Jean-Pierre STEIN <stein.jean-pierre@mail.be>
2022-09-02 23:00:08 +0200

    test

--- main.py
+++ main.py
@@ -8,23 +8,26 @@ import re
 # Variables
 WEBSITE = "http://books.toscrape.com/"
 # type the book of url book
-urlbook = "http://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html"
-#urlbook = "http://books.toscrape.com/catalogue/a-flight-of-arrows-the-pathfinders-2_876/index.html"
+URLBOOK = ["http://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html",
+           "http://books.toscrape.com/catalogue/a-flight-of-arrows-the-pathfinders-2_876/index.html",
+           "http://books.toscrape.com/catalogue/the-bachelor-girls-guide-to-murder-herringford-and-watts-mysteries-1_491/index.html"]
 
-HEADER_CSV = ["product_page_url","universal_ product_code (upc)","title","price_including_tax","price_excluding_tax",
+URLCATEGORY = ["http://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html"]
+
+HEADER_CSV = ["product_page_url","universal_ product_code (upc)","title","price_including_tax (£)","price_excluding_tax (£)",
               "number_available","product_description","category","review_rating","image_url"]
 
 COLUMNS_URL = {"universal_ product_code (upc)" :"UPC",
                "title" : "col-sm-6 product_main",
-               "price_including_tax" : "Price (incl. tax)",
-               "price_excluding_tax" : "Price (excl. tax)",
+               "price_including_tax (£)" : "Price (incl. tax)",
+               "price_excluding_tax (£)" : "Price (excl. tax)",
                "number_available" : "Availability",
                "product_description" : "Product Description",
                "category" : "books.toscrape.com/catalogue/category/books/",
                "review_rating" : "Number of reviews",
                "image_url" : "image_container"}
 
-# Fonctions azerty
+# Fonctions
 def search_col(th,td,col_url,cols,ind):
     str = ""
     i = 0
@@ -48,10 +51,10 @@ def analyze_url_book(url,col_url,cols):
     page = rq.get(url)
 
     soup = bs(page.content, 'html.parser')
-   # print(soup.find_all('ul',class_="breadcrumb"))
-    #print(soup.find_all('p', class_="star-rating"))
-    print(soup.find("meta",  attrs={'name':'description'}).get('content'))
-
+    # print(soup.find_all('ul',class_="breadcrumb"))
+    #print(soup.find('p', class_="star-rating"))
+    for div in soup.find_all('div', 'thumbnail'):
+        img = div.find('img', alt=True)
     # upc ,price , number available
     th = soup.find_all('th')
     td = soup.find_all('td')
@@ -61,18 +64,16 @@ def analyze_url_book(url,col_url,cols):
     simple_line.append(search_col(th, td, col_url, cols, 1))
     # add title
     #h1 = soup.find_all('h1')
-    h1 = soup.find_all('li', class_="active")
-    for title in h1:
-        simple_line.append(title.string)
+    simple_line.append(img['alt'])
     # add price_excluding_tax
-    simple_line.append(search_col(th, td, col_url, cols, 4))
+    simple_line.append(search_col(th, td, col_url, cols, 4).replace("£",''))
     # add price_including_tax
-    simple_line.append(search_col(th, td, col_url, cols, 3))
+    simple_line.append(search_col(th, td, col_url, cols, 3).replace("£",''))
     # add number_available
     nbr_books = search_col(th, td, col_url, cols, 5)
     simple_line.append(re.sub("[^0-9]", "", nbr_books))
     # add description
-    simple_line.append(soup.find("meta",  attrs={'name':'description'}).get('content'))
+    simple_line.append(soup.find("meta",  attrs={'name':'description'}).get('content').strip())
     # add category
     categ = soup.find_all('a', href=True)  # category
     i = 1
@@ -82,9 +83,21 @@ def analyze_url_book(url,col_url,cols):
             break
         else:
             i = i + 1
+    # add review rating
+    simple_line.append(search_col(th, td, col_url, cols, 8))
+    # add url image
+    simple_line.append(img['src'].replace("../../",WEBSITE))
 
     return simple_line
 
+# analyze category
+def analyze_url_category(url,col_url,cols):
+    simple_tab = []
+    page = rq.get(url)
+
+    soup = bs(page.content, 'html.parser')
+
+    return simple_tab
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     try:
@@ -93,10 +106,18 @@ if __name__ == '__main__':
     except HTTPError as hp:
         print(hp)
 
-    # create one csv for one book
-    df = pd.DataFrame(columns=HEADER_CSV)
+    # create one csv for one or several books
+    df_book = pd.DataFrame(columns=HEADER_CSV)
 
-    #df.loc[len(df)] = analyze_url_book(urlbook,COLUMNS_URL)
-    sl = analyze_url_book(urlbook,COLUMNS_URL,HEADER_CSV)
-    print(sl)
-    #print(df)
+    for urlbook in URLBOOK:
+        df_book.loc[len(df_book)] = analyze_url_book(urlbook,COLUMNS_URL,HEADER_CSV)
+    # create csv
+    df_book.to_csv('books.csv', index=False,sep=';', encoding='utf-8')
+
+    # create one csv for one category
+    df_category = pd.DataFrame(columns=HEADER_CSV)
+
+    for urlcategory in URLCATEGORY:
+        df_category.loc[len(df_book)] = analyze_url_category(urlcategory, COLUMNS_URL, HEADER_CSV)
+    # create csv
+    df_category.to_csv('books.csv', index=False, sep=';', encoding='utf-8')
